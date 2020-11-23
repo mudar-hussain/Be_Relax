651c257ad746a5478a98a40b30b4f236822c9712
mudar-hussain <v448474@gmail.com>
2020-11-23 11:59:24 +0530

    Javascript added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = () => {
     const song = document.querySelector('.song');
     const play = document.querySelector('.play');
     const outline = document.querySelector('.moving-outline circle');
-
+ 
     //Sounds
     const sounds = document.querySelectorAll('.sound-picker button');
     //Time Display
